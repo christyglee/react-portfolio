4defc340f24429ae628bc28ab4ef98f5991a0384
unknown <christy.g.lee@gmail.com>
2020-11-18 20:37:18 -0800

    image and intro container

---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+const Home = ({name, paragraph, paragraph2, authorImage})=>{
+
+    return(
+    <div>
+        <div className="home_container">
+            <div className="home_head_wrapper">
+            <h1> Hello, I 'm <br></br> <span>{name}</span></h1>
+            <p>{paragraph} <br></br>{paragraph2}</p>
+            </div>
+            <div className="image_container">
+                <img src={authorImage} alt="Author"></img>
+                <div className="bg"></div>
+            </div>
+            
+        </div>
+    </div>
+    )
+}
+
+export default Home;
