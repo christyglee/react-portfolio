26e94b6b3a130a55508f834885db603fb9486fbd
unknown <christy.g.lee@gmail.com>
2020-11-18 20:43:31 -0800

    about me component/page

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from './header';
+import Contact from './Contact';
+import data from './info';
+import '../App.css';
+// import Footer from './Footer';
+
+const About = () =>{
+return (
+  <div>
+    <Header name={data.name} contactEmail={data.contactEmail}></Header>
+    <div className="about_container">
+      <h1>{data.aboutTitle}</h1>
+      <p>{data.aboutPara[0]}</p>
+      <p>{data.aboutPara[1]}</p>
+      <p>{data.aboutPara[2]}</p>
+      <p>{data.aboutPara[3]}</p>
+      <p>{data.aboutPara[4]}</p>
+      </div>   
+
+      <div className="about_contact">
+        <Contact
+          contactEmail={data.contactEmail}
+          contactPara={data.contactPara}
+          socialLinks={data.social}
+        ></Contact>
+        
+      </div>
+
+  </div>
+);
+}
+export default About;
