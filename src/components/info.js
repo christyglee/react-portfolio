0b70325c3e3b294e86bd53ff665a697614a037b1
unknown <christy.g.lee@gmail.com>
2020-11-18 20:40:56 -0800

    personal info component/array

---
+++ src/components/info.js
@@ -0,0 +1,101 @@
+export default {
+
+  name: "CHRISTY LEE",
+
+  // Contact Email
+  contactEmail: "christy.g.lee@gmail.com",
+
+  // Landing page name
+  landingPageName: "Christy Lee",
+
+  // Landing page text
+  landingPagePara: [
+    'Aspiring Full-Stack Web Developer',
+    'Making your vision come to life.',
+  ],
+
+  // Landing Page Image (replace - root/public/assets/photo.jpg)
+  landingPageImage: "images/headshot.png",
+
+  // Project information
+  projects: [
+    {
+      id: 1, 
+      title: "o'clocktail repository", 
+
+      imageSrc: "images/o'clocktail_website.PNG",
+
+      url: "https://christyglee.github.io/o-clocktail/",
+      repoUrl: "https://github.com/christyglee/o-clocktail"
+    },
+    {
+      id: 2, 
+      title: "weather dashboard repository",
+      imageSrc: "images/weather_dashboard.PNG",
+      url: "https://christyglee.github.io/weather-dashboard/",
+      repoUrl: "https://github.com/christyglee/weather-dashboard"
+    },
+    {
+      id: 3, 
+      title: "work day scheduler repository",
+      imageSrc: "images/Day_Planner.jpg",
+      url: "https://christyglee.github.io/work-day-scheduler/",
+      repoUrl: "https://github.com/christyglee/work-day-scheduler"
+    },
+    {
+      id: 4, 
+      title: "burger repository",
+      imageSrc: "images/Burger-App.png",
+      url: "https://fathomless-everglades-84577.herokuapp.com/",
+      repoUrl: "https://github.com/christyglee/burger"
+    },
+    {
+      id: 5, 
+      title: "rooTs repository",
+      imageSrc: "images/roots-App.png",
+      url: "https://infinite-cove-31054.herokuapp.com/",
+      repoUrl: "https://github.com/christyglee/roots-a-social-garden"
+    },
+    // {
+    //   id: 6, 
+    //   title: "rooTs a social media garden repository",
+    //   imageSrc: "images/roots-App.png",
+    //   url: "https://infinite-cove-31054.herokuapp.com/",
+    //   repoUrl: "https://github.com/christyglee/roots-a-social-garden"
+    // }
+
+  ],
+
+  // Contact information
+  contactPara:
+    "contact information",
+  social: [
+
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/christy-g-lee"
+    },
+    {
+      name: "Github",
+      url: "https://github.com/christyglee"
+    },
+    {
+      name: "Resume",
+      url: "images/Christy_Lee_Resume_Oct2020.pdf"
+    }
+  ],
+
+  // About page
+  aboutTitle: "about me.",
+  aboutPara:[
+    'Greetings and welcome to my portfolio! My name is Christy Lee, I was born and raised in sunny San Diego and now currently residing in Orange County after living in San Francisco for a couple of years. Some of my hobbies include baking, hiking, traveling, eating, and snuggling with my cat, Louis Kitzstrong (Louis for short).',
+
+    'I attended San Diego State University and obtained a Bachelor of Science degree in Hospitality & Tourism Management with emphasis in Hotel Management. Shortly after graduating, I started my career with Marriott International and was with the company for over 8 years.',
+  
+    'My interest in coding stems back from the Xanga and Myspace days (I may be dating myself here, haha), but I never thought about pursuing coding as a career nor had the time to learn post graduating. This pandemic has been a blessing in disguise as it has given me the time and opportunity to pursue coding as my new career path in the tech industry.',
+  
+    'As I am currently enrolled in a Full-Stack Web Development coding boot camp through UC Berkeley, I have an interest in becoming either a Full-Stack web developer or a Front-End web designer; however, the options are limitless!',
+  
+    'Thank you for taking the time to view my portfolio! Please feel free to reach out via email or connect with me via LinkedIn and GitHub!'
+  ]
+};
