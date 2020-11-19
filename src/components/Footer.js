2479fd0014c6c152ba3efdf442f20e96e09eb730
unknown <christy.g.lee@gmail.com>
2020-11-18 20:39:01 -0800

    footer component, not finished

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+  
+// import React from "react";
+// import '../App.css';
+
+// function Footer(props) {
+//     return (
+//         // Sticky footer template
+//         <footer className="footer mt-auto py-3 text-center">
+//             <p>Copyright 2020</p>
+//         </footer>
+//     )
+// }
+
+// export default Footer;
