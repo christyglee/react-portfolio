0a094a5552684e5f1d286b29e33a4b99227ffc8a
Christy Lee <christy.g.lee@gmail.com>
2021-06-23 14:54:44 -0700

    updated href link to direct to let's get social

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,9 @@ const Header = ({name, contactEmail})=>{
                 <nav>
                     <ul>
                         <li><Link to="/about">ABOUT</Link></li>
-                        <li><a href={`mailto:${contactEmail}`}>CONTACT</a></li>
+                        {/* shortcut to contacts */}
+                        <li><a href={`./react-portfolio/#contact`}>CONTACT</a></li>
+                        {/* <li><a href={`mailto:${contactEmail}`}>CONTACT</a></li> */}
                     </ul>
                 </nav>
             </div>
